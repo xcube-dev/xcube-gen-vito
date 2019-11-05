f5925b0edc9597aa7d58ddb6efe1271e796cf9fb
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2019-11-05 12:11:09 +0100

    fixing plugin test

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -1,11 +1,14 @@
 import unittest
 
-from xcube_gen_vito.iproc import init_plugin
+from xcube.util import extension
+from xcube_gen_vito.plugin import init_plugin
 
 
 class VitoS2PlusPluginTest(unittest.TestCase):
 
     # noinspection PyMethodMayBeStatic
     def test_init_plugin(self):
-        # Smoke test
-        init_plugin()
+        ext_reg = extension.ExtensionRegistry()
+        init_plugin(ext_reg)
+        self.assertTrue(ext_reg.has_extension('xcube.core.gen.iproc', 'vito-s2plus-l2'))
+
